f3d70694cd9be9b7677dfccea26e6034200103af
shadoW <uhsnarpggupta@gmail.com>
2017-07-02 10:53:44 +0530

    Added Files {1}

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// var mongo = require('mongodb');
+
+var index = require('./routes/index');
+var restaurant = require('./routes/restaurant');
+var shopping = require('./routes/shopping');
+var hotel = require('./routes/hotel');
+var aboutus = require('./routes/about');
+var event = require('./routes/event');
+// var admin = require('./routes/admin');
+var swadeshi = require('./routes/shopping/swadeshi');
+var sheeshMahal = require('./routes/shopping/sheeshmahal');
+var raymonds = require('./routes/shopping/raymonds');
+var adidas = require('./routes/shopping/adidas');
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Home Page Routes
+app.use('/', index);
+app.use('/restaurant',restaurant);
+app.use('/shop',shopping);
+app.use('/hotel',hotel);
+app.use('/about',aboutus);
+app.use('/event',event);
+// app.use('/admin',admin);
+
+//Shopping Routes
+app.use('/swadeshi',swadeshi);
+app.use('/sheeshmahal',sheeshMahal);
+app.use('/raymonds',raymonds);
+app.use('/adidas',adidas);
+
+
+//Admin Routes
+// app.use('/insert',admin);
+// app.use('/get-data',admin);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Page Not Found');
+  err.status = 404;
+  console.log(req.url + " This is URL");
+  console.log(app.get('views') + " This is the View");
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
