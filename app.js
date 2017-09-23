d825273d6e574ec5b4615613820534790bafea5e
msvarshney <msvarshney@gmail.com>
2017-09-23 12:24:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -146,4 +146,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
 module.exports = app;
